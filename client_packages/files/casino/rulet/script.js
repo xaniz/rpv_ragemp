ba3f7c09f54e90437e601104eacc838b44896ce3
Xaniz <59030822+xaniz@users.noreply.github.com>
2024-01-07 22:19:06 +0100

    Lucky Wheel
    Now you can choose the amount of money

--- client_packages/files/casino/rulet/script.js
+++ client_packages/files/casino/rulet/script.js
@@ -1,147 +1,155 @@
-var options = ["100", "10", "25", "250", "0", "1000", "1", "200", "45", "500", "5", "20", "0", "10", "0", "350", "5", "10", "50", "30"];
+document.addEventListener('DOMContentLoaded', function() {
+  var options = ["0", "1X", "0", "2X", "0", "3X", "0", "0", "5x", "0", "5x", "0", "1x", "0", "2x", "0", "3x", "0", "1x", "0"];
+  var startAngle = 0;
+  var arc = Math.PI / (options.length / 2);
+  var isSpinning = false;
+  var spinAngleStart = 10;
+  var spinTime = 0;
+  var spinTimeTotal = 0;
+  var ctx;
 
-var startAngle = 0;
-var arc = Math.PI / (options.length / 2);
-var spinTimeout = null;
+  var betAmountInput = document.getElementById("betAmount");
 
-var spinArcStart = 10;
-var spinTime = 0;
-var spinTimeTotal = 0;
-
-var ctx;
-
-document.getElementById("spin").addEventListener("click", spin);
-document.getElementById("exit").addEventListener("click", exit);
-
-function byte2Hex(n) {
-  var nybHexString = "0123456789ABCDEF";
-  return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
-}
-
-function RGB2Color(r,g,b) {
-	return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
-}
-
-function getColor(item, maxitem) {
-  var phase = 0;
-  var center = 128;
-  var width = 127;
-  var frequency = Math.PI*2/maxitem;
-  
-  red   = Math.sin(frequency*item+2+phase) * width + center;
-  green = Math.sin(frequency*item+0+phase) * width + center;
-  blue  = Math.sin(frequency*item+4+phase) * width + center;
-  
-  return RGB2Color(red,green,blue);
-}
-
-function drawRouletteWheel() {
   var canvas = document.getElementById("canvas");
   if (canvas.getContext) {
+      var outsideRadius = 200;
+      var textRadius = 160;
+      var insideRadius = 125;
+
+      ctx = canvas.getContext("2d");
+  }
+
+  function byte2Hex(n) {
+      var nybHexString = "0123456789ABCDEF";
+      return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
+  }
+
+  function RGB2Color(r, g, b) {
+      return '#' + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
+  }
+
+  function getColor(item, maxitem) {
+      var phase = 0;
+      var center = 128;
+      var width = 127;
+      var frequency = Math.PI * 2 / maxitem;
+      
+      var red = Math.sin(frequency * item + 2 + phase) * width + center;
+      var green = Math.sin(frequency * item + 0 + phase) * width + center;
+      var blue = Math.sin(frequency * item + 4 + phase) * width + center;
+      
+      return RGB2Color(red, green, blue);
+  }
+
+  function drawRouletteWheel() {
     var outsideRadius = 200;
     var textRadius = 160;
     var insideRadius = 125;
 
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0,0,500,500);
-
+    ctx.clearRect(0, 0, 500, 500);
     ctx.strokeStyle = "black";
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2;
 
-    ctx.font = 'bold 15px Helvetica, Arial';
+    for (var i = 0; i < options.length; i++) {
+        var angle = startAngle + i * arc;
+        ctx.fillStyle = getColor(i, options.length);
 
-    for(var i = 0; i < options.length; i++) {
-      var angle = startAngle + i * arc;
-      //ctx.fillStyle = colors[i];
-      ctx.fillStyle = getColor(i, options.length);
+        ctx.beginPath();
+        ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+        ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+        ctx.fill();
+        ctx.stroke();
 
+        ctx.fillStyle = "black";
+        ctx.save();
+        ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 
+                        250 + Math.sin(angle + arc / 2) * textRadius);
+        ctx.rotate(angle + arc / 2 + Math.PI / 2);
+        var text = options[i];
+        ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+        ctx.restore();
+    }
+
+    // Arrow
+      ctx.fillStyle = "red";
       ctx.beginPath();
-      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
+      ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+      ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
+      ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
+      ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
+      ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
+      ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
       ctx.fill();
-
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur    = 0;
-      ctx.shadowColor   = "rgb(255, 0, 0)";
-      ctx.fillStyle = "white";
-      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 
-                    250 + Math.sin(angle + arc / 2) * textRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      var text = options[i];
-      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-      ctx.restore();
-    } 
-
-    //Arrow
-    ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
-    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.fill();
-  }
-}
-
-let canSpin = true;
-
-function spin() {
-  if (!canSpin) {
-    return;
   }
 
-  canSpin = false;
-  setTimeout(() => {
-    canSpin = true;
-  }, 10000);
-
-  spinAngleStart = Math.random() * 10 + 10;
-  spinTime = 0;
-  spinTimeTotal = Math.random() * 3 + 4 * 2000;
-  mp.trigger("client::ruletzavrti", 100);
-  rotateWheel();
-}
-
-function exit() {
-  mp.trigger("Hide_Crafting_System");
-}
-
-function rotateWheel() {
-  spinTime += 30;
-  if(spinTime >= spinTimeTotal) {
-    stopRotateWheel();
-    return;
+  function rotateWheel() {
+      spinTime += 30;
+      if (spinTime >= spinTimeTotal) {
+          stopRotateWheel();
+          return;
+      }
+      var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+      startAngle += (spinAngle * Math.PI / 180);
+      drawRouletteWheel();
+      requestAnimationFrame(rotateWheel);
   }
-  var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-  startAngle += (spinAngle * Math.PI / 180);
+
+  function stopRotateWheel() {
+    var degrees = startAngle * 180 / Math.PI + 90;
+    var arcd = arc * 180 / Math.PI;
+    var index = Math.floor((360 - degrees % 360) / arcd);
+    var multiplierText = options[index];
+    var multiplier = multiplierText === "0" ? 0 : parseFloat(multiplierText.replace('x', ''));
+    var betAmountInput = document.getElementById("betAmount");
+    var betAmount = parseInt(betAmountInput.value, 10); 
+    if (!Number.isInteger(betAmount) || betAmount <= 0) {
+        return;
+    }
+
+    var totalWin = betAmount * multiplier;
+
+    ctx.save();
+    ctx.font = 'bold 30px Helvetica, Arial';
+    var text = multiplier > 0 ? "Win: $" + totalWin.toFixed(2) : "Try Again!";
+    ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
+    ctx.restore();
+    
+    mp.trigger("client::ruletpobeda", totalWin);
+    isSpinning = false;
+}
+
+  function easeOut(t, b, c, d) {
+      var ts = (t /= d) * t;
+      var tc = ts * t;
+      return b + c * (tc + -3 * ts + 3 * t);
+  }
+
+  document.getElementById("spin").addEventListener("click", function() {
+
+    if (isSpinning) {
+      return;
+  }
+    var betAmountInput = document.getElementById("betAmount");
+    var betAmount = parseInt(betAmountInput.value, 10);
+    
+    if (!Number.isInteger(betAmount) || betAmount <= 0) {
+        return;
+    }
+    
+    mp.trigger("client::ruletzavrti", betAmount);
+    isSpinning = true;
+    
+    spinAngleStart = Math.random() * 10 + 10;
+    spinTime = 0;
+    spinTimeTotal = Math.random() * 3 + 4 * 1000;
+    rotateWheel();
+});
+
+  document.getElementById("exit").addEventListener("click", function() {
+      mp.trigger("Hide_Crafting_System");
+  });
+
   drawRouletteWheel();
-  spinTimeout = setTimeout('rotateWheel()', 30);
-}
-
-function stopRotateWheel() {
-  clearTimeout(spinTimeout);
-  var degrees = startAngle * 180 / Math.PI + 90;
-  var arcd = arc * 180 / Math.PI;
-  var index = Math.floor((360 - degrees % 360) / arcd);
-  ctx.save();
-  ctx.font = 'bold 30px Helvetica, Arial';
-  var text = options[index]
-  ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
-  mp.trigger("client::ruletpobeda", text);
-  ctx.restore();
-}
-
-function easeOut(t, b, c, d) {
-  var ts = (t/=d)*t;
-  var tc = ts*t;
-  return b+c*(tc + -3*ts + 3*t);
-}
-
-drawRouletteWheel();
+});
